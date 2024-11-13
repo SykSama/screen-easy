b5117646e0be01a7073aee76890f0534b89f2612
SykSama <syksama@gmail.com>
2024-11-13 10:13:07 +0700

    fix(org): add comment to do it later

--- app/orgs/[orgSlug]/layout.tsx
+++ app/orgs/[orgSlug]/layout.tsx
@@ -1,7 +1,8 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
 import type { LayoutParams } from "@/types/next";
 
 export default async function RouteLayout(
   props: LayoutParams<{ orgSlug: string }>,
 ) {
-  return <div>{props.children}</div>;
+  return <SidebarProvider>{props.children}</SidebarProvider>;
 }
--- app/orgs/new/page.tsx
+++ app/orgs/new/page.tsx
@@ -2,6 +2,10 @@ import { requiredAuth } from "@/features/auth/helper";
 import { NewOrgForm } from "./new-org-form";
 
 export default async function NewOrgPage() {
+  //TODO: Add default param in organization
+  //TODO: Add filter in sync with url
+  //TODO: better UI
+
   await requiredAuth();
   return <NewOrgForm />;
 }
