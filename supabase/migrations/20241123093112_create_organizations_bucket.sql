925720ae6e8c950b01c995fecd0c7fa7aa3e08bd
SykSama <syksama@gmail.com>
2024-11-23 17:05:51 +0700

    chore(org-bucket): create organization bucket

---
+++ supabase/migrations/20241123093112_create_organizations_bucket.sql
@@ -0,0 +1,26 @@
+-- Use Postgres to create a bucket.
+insert into storage.buckets (id, name, public) 
+values('organizations', 'organizations', false);
+
+CREATE POLICY "Organization members can view their files" ON storage.objects
+FOR SELECT TO authenticated
+USING (
+  bucket_id = 'organizations' AND
+  (storage.foldername(name))[1] IN (
+    SELECT organization_id::text 
+    FROM organization_memberships 
+    WHERE profile_id = auth.uid()
+  )
+);
+
+-- Storage bucket policy
+CREATE POLICY "Organization members can upload files" ON storage.objects
+FOR INSERT TO authenticated
+WITH CHECK (
+  bucket_id = 'organizations' AND
+  (storage.foldername(name))[1] IN (
+    SELECT organization_id::text 
+    FROM organization_memberships 
+    WHERE profile_id = auth.uid()
+  )
+);
