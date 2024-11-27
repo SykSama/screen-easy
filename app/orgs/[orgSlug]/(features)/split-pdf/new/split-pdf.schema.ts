cd1d21f1e2910ed1477b2a07c0def8b82f343515
SykSama <syksama@gmail.com>
2024-11-27 08:04:40 +0700

    refactor(split-pdf): ♻️ change to have a schema instead of global refine

--- app/orgs/[orgSlug]/(features)/split-pdf/new/split-pdf.schema.ts
+++ app/orgs/[orgSlug]/(features)/split-pdf/new/split-pdf.schema.ts
@@ -1,41 +1,11 @@
+import { splittingProcessingConfigSchema } from "@/features/jobs/types";
 import { z } from "zod";
 import { zfd } from "zod-form-data";
 
 export const SplitPdfSchema = zfd
   .formData({
     files: z.array(zfd.file()),
-    splittingMethod: z.enum(["bookmarks", "ai"]),
-    aiSplittingCriteria: z.string().optional(),
-    renamingMethod: z.enum(["manual", "ai"]),
-    manualRenameTemplate: z.string().optional(),
-    aiRenamingCriteria: z.string().optional(),
-    aiRenamingExample: z.string().optional(),
   })
-  .refine(
-    (data) => {
-      if (data.splittingMethod === "ai") {
-        return !!data.aiSplittingCriteria;
-      }
-
-      return true;
-    },
-    {
-      message:
-        "AI splitting criteria is required when using AI splitting method",
-    },
-  )
-  .refine(
-    (data) => {
-      switch (data.renamingMethod) {
-        case "manual":
-          return !!data.manualRenameTemplate;
-        case "ai":
-          return !!data.aiRenamingCriteria && !!data.aiRenamingExample;
-        default:
-          return true;
-      }
-    },
-    { message: "Required fields missing for selected renaming method" },
-  );
+  .and(splittingProcessingConfigSchema);
 
 export type SplitPdfFormValues = z.infer<typeof SplitPdfSchema>;
