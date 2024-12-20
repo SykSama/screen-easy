23722a29cbccb1dd9d9f5859693ddd578443b180
SykSama <syksama@gmail.com>
2024-12-20 12:09:28 +0700

    feat(bucket): add rls for service_account

--- supabase/migrations/20241123093112_create_organizations_bucket.sql
+++ supabase/migrations/20241123093112_create_organizations_bucket.sql
@@ -36,3 +36,39 @@ USING (
     WHERE profile_id = auth.uid()
   )
 );
+
+-- service account can view their files
+create policy "Organization service account can view their files" on storage.objects
+for select to service_account
+using (
+  bucket_id = 'organizations' AND
+  (storage.foldername(name))[1] IN (
+    select organization_id::text 
+    from service_accounts 
+    where id = auth.uid()
+  )
+);
+
+-- Storage bucket policy
+-- create policy "Organization service account can upload files" on storage.objects
+-- for insert to service_account
+-- WITH CHECK (
+--   bucket_id = 'organizations' AND
+--   (storage.foldername(name))[1] IN (
+--     select organization_id::text 
+--     from service_accounts 
+--     where id = auth.uid()
+--   )
+-- );
+
+-- -- Storage bucket policy
+-- create policy "Organization service account can update files" on storage.objects
+-- for update to service_account
+-- using (
+--   bucket_id = 'organizations' AND
+--   (storage.foldername(name))[1] IN (
+--     select organization_id::text 
+--     from service_accounts 
+--     where id = auth.uid()
+--   )
+-- );
